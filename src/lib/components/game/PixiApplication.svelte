800609dde60246a63b464e2cbb93bfcd2c0d8bbd
joshsurin <joshu@LAPTOP-VGCCJ81Q>
2024-03-01 09:26:25 +1100

    pixiapp in store

--- src/lib/components/game/PixiApplication.svelte
+++ src/lib/components/game/PixiApplication.svelte
@@ -1,39 +1,17 @@
 <script lang="ts">
-    import * as PIXI from 'pixi.js';
+    import { onMount } from 'svelte';
+    import { createApp, pixiApplication } from '$lib/utils/game/createApp';
 
-    import { DEVICE_RATIO } from '$lib/utils/game/utils';
+    onMount(() => {
+        createApp()
+    })
 
-    let { children }: any = $props();
 
-    function createApp() {
-        let pixiApplication = $state(new PIXI.Application<HTMLCanvasElement>({
-        backgroundAlpha: 1,
-        autoDensity: true,
-        resolution: DEVICE_RATIO,
-        antialias: true,
-        width: 400,
-        height: 700,
-        }))
-
-        globalThis.__PIXI_APP__ = pixiApplication;
-
-        // to prevent that you can't scroll the page with touch on the canvas. https://github.com/pixijs/pixijs/issues/4824
-        pixiApplication.renderer.events.autoPreventDefault = false;
-        pixiApplication.renderer.view.style.touchAction = 'auto';
-
-        document.body.appendChild(pixiApplication.view)
-
-        return {
-            get pixiApplication() { return pixiApplication },
-        };
-
-    }
-
-  export const app = createApp();
+    $: console.log($pixiApplication)
 </script>
 
-{#if app.pixiApplication}
-    {@render children()}
+{#if $pixiApplication}
+    <slot />
 {/if}
 
 <style>
--- src/lib/utils/game/createApp.ts
+++ src/lib/utils/game/createApp.ts
@@ -1,4 +1,30 @@
-import app from '$lib/components/game/PixiApplication.svelte';
+import * as PIXI from 'pixi.js';
+import { writable } from 'svelte/store';
+import { DEVICE_RATIO } from '$lib/utils/game/utils';
 
-console.log(app)
+export const pixiApplication: any = writable()
+
+export function createApp() {
+    const app = new PIXI.Application<HTMLCanvasElement>({
+        backgroundAlpha: 1,
+        autoDensity: true,
+        resolution: DEVICE_RATIO,
+        antialias: true,
+        width: 400,
+        height: 700,
+    })
+
+    globalThis.__PIXI_APP__ = app;
+
+    // to prevent that you can't scroll the page with touch on the canvas. https://github.com/pixijs/pixijs/issues/4824
+    app.renderer.events.autoPreventDefault = false;
+    app.renderer.view.style.touchAction = 'auto';
+
+    document.body.appendChild(app.view)
+    pixiApplication.set(app)
+
+    return () => {
+        app.destroy();
+    };
+}
 
