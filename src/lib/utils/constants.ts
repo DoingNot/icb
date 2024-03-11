77b8b0c94d00d8c88cacfc56bcb37968f1102379
joshu <josh@DESKTOP-VTNNM6C.localdomain>
2024-03-11 22:58:00 +1100

    blur sprites

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -46,10 +46,10 @@ export const BAR_STROKE_COLOR = '#464542'
 export const BAR_LINE_WIDTH = 3
 export const BAR_DROPSHADOW_OPTIONS = {
     'alpha': 0.65,
-    'blur': 3,
+    'blur': 2,
     'color': 0x1A160A,
     'offset': { x: 0, y: 10 },
-    'quality': 10
+    'quality':5 
 }
 
 export const BALL_SIZE = 18 * mobileRatio
@@ -60,10 +60,11 @@ export const BALL_COLOR = '#BFBCB3'
 export const BALL_STROKE_COLOR = '#464542'
 export const BALL_LINE_WIDTH = 3
 export const BALL_DROPSHADOW_OPTIONS = {
-    'alpha': 0.65,
+    'alpha': 0.35,
     'color': 0x1A160A,
-    'offset': { x: 0, y: 9 },
-    'quality': 10
+    'offset': { x: 0, y: 6 },
+    'quality': 5,
+    'blur': 1
 }
 
 export const ASSETS_DATA = [
